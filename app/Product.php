d7b118f8bfaed38f5a3881554b385b5cc1f748bc
tuna62@wru.vn <38431407+AnhTuNguyen30198@users.noreply.github.com>
2020-06-28 21:17:28 +0700

    rank again
    again

---
+++ app/Product.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+  protected $table = 'products';
+  protected $fillable = [
+    'product_name',  'product_counts',  'products_type',  'product_in_prices',  'product_out_prices'
+  ];
+}
