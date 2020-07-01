0ea1732777e188ca067424550ee7893b29a11fd7
tuna62@wru.vn <38431407+AnhTuNguyen30198@users.noreply.github.com>
2020-07-01 17:22:14 +0700

    fix sale
    slae

--- database/seeds/SaleSeeder.php
+++ database/seeds/SaleSeeder.php
@@ -11,20 +11,10 @@ class SaleSeeder extends Seeder
      */
     public function run()
     {
-        $product_name = ['Iphone6','Ipad Air 2','Iphone XS max','Airpods Pro','Macbook Pro 2020','Iphone 8'];
-        $product_name2 = ['Asus VivoBook X409FA','Asus VivoBook A512FA','Asus ZenBook Edition 30 ',
-            'Asus Gaming ROG Strix G531 ',
-            'Asus ZenBook 13 ','Asus VivoBook S430FN ',
-            'Asus VivoBook S530FN '];
-        $product_name3 = ['IdeaPad Slim 3 ','  Flex 2 Pro 15 (Broadwell)','Flex 2 Pro 15 (Haswell)'
-            ,'Yoga 3 11 ','Z41-70/Z51-70','S41-70/U41-70','G50-80 Touch'];
-        $product_name4 = ['Dell Insprion 3212','Dell Vostro 5400','Dell Latitude 2312','Dell XPS 2221',
-            'Dell Precision 32','Dell XPS 15','Dell Precision 7000'];
-        $product_name5 = ['HP Pavilion tx2000','HP G 4321','HP Envy 14','HP Essentials 1234','HP ProBook 15','HP Elitebook 16','HP Mini 210'];
         for($i=0;$i<6;$i++)
         {
             $data = [
-                'sale_product_name' =>$product_name[$i],
+                'sale_percent' =>rand(1,40),
                 'sale_product_type'=>'APPLE',
                 'sale_begin' => now(),
                 'sale_end'=> now(),
@@ -35,7 +25,7 @@ class SaleSeeder extends Seeder
         for($i=0;$i<6;$i++)
         {
             $data = [
-                'sale_product_name' =>$product_name2[$i],
+                'sale_percent' =>rand(1,40),
                 'sale_product_type'=>'ASUS',
                 'sale_begin' => now(),
                 'sale_end'=> now(),
@@ -46,7 +36,7 @@ class SaleSeeder extends Seeder
         for($i=0;$i<6;$i++)
         {
             $data = [
-                'sale_product_name' =>$product_name3[$i],
+                'sale_percent' =>rand(1,40),
                 'sale_product_type'=>'LENOVO',
                 'sale_begin' => now(),
                 'sale_end'=> now(),
@@ -57,7 +47,7 @@ class SaleSeeder extends Seeder
         for($i=0;$i<6;$i++)
         {
             $data = [
-                'sale_product_name' =>$product_name4[$i],
+                'sale_percent' =>rand(1,40),
                 'sale_product_type'=>'DELL',
                 'sale_begin' => now(),
                 'sale_end'=> now(),
@@ -68,7 +58,7 @@ class SaleSeeder extends Seeder
         for($i=0;$i<6;$i++)
         {
             $data = [
-                'sale_product_name' =>$product_name5[$i],
+                'sale_percent' =>rand(1,40),
                 'sale_product_type'=>'HP',
                 'sale_begin' => now(),
                 'sale_end'=> now(),
