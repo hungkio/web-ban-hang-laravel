2bd4bfda744b0df8058b864673b06bceef8491ba
tuna62@wru.vn <38431407+AnhTuNguyen30198@users.noreply.github.com>
2020-07-02 13:39:28 +0700

    config Seeder
    Ok

--- database/factories/GuestFactory.php
+++ database/factories/GuestFactory.php
@@ -1,13 +1,14 @@
 <?php
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 use Faker\Generator as Faker;
+use App\Guest;
 
 $factory->define(App\Guest::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'address' => $faker->address,
         'phonenumber' => $faker->phoneNumber,
-        'sex' => $faker->biasedNumberBetween(1,2),
+        'sex' => $faker->biasedNumberBetween(Guest::GENDER_CODE['male'],Guest::GENDER_CODE['female']),
         'email' => $faker->unique()->safeEmail,
         'created_at' => new DateTime,
         'updated_at' => new DateTime,
